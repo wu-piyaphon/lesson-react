c2e30e1d7c8e5c3f994a4cdb76a8f5b76ce6d394
Piyaphon Wu <wpiyaphon@gmail.com>
2025-11-16 12:26:29 +0700

    Change HeaderSection layout by adjusting position to display properly

--- sections/header/header-section.tsx
+++ sections/header/header-section.tsx
@@ -4,10 +4,14 @@ import HeaderText from "./header-text";
 
 export default function HeaderSection() {
   return (
-    <Container className="grid grid-cols-1 items-center justify-center gap-x-8 gap-y-16 lg:grid-cols-2">
-      <HeaderText />
+    <div className="-mt-19 -mb-32 overflow-hidden pt-19 pb-32">
+      <div className="py-16 sm:px-2 lg:relative lg:px-0 lg:py-20">
+        <Container className="grid grid-cols-1 items-center justify-center gap-x-8 gap-y-16 py-0 lg:grid-cols-2 lg:py-0">
+          <HeaderText />
 
-      <HeaderCode />
-    </Container>
+          <HeaderCode />
+        </Container>
+      </div>
+    </div>
   );
 }
